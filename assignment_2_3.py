f8ec8b56da20205cd46297e2e8a39db22566bfc9
Bobby Kimani <bobbykimani@Bobbys-MacBook-Pro.local>
2021-10-22 11:47:09 +0300

    Use of Loop in string

---
+++ assignment_2_3.py
@@ -0,0 +1,16 @@
+#Using loop to print my name 20 times
+
+#assignment_2_3.py
+
+
+def main():
+
+    print("This is a loop program to display entered name(My name) 20 times")
+
+    name = input("Please enter your full names:")
+
+    for i in range(20):
+        
+        print("Your name is:",name)
+
+main()
