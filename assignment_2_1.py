b19e4b20d9250151989d5f74de960769e3d4fa18
Bobby Kimani <bobbykimani@Bobbys-MacBook-Pro.local>
2021-10-22 11:28:13 +0300

    Practice of calculations

---
+++ assignment_2_1.py
@@ -0,0 +1,63 @@
+#assignment_2_1.py
+
+def addition():
+    print("This adds 2 to a")
+
+    a = int(input("Please enter the value of a:"))
+
+    b = a +2
+
+    print("The value of b is:",b)
+
+addition()
+
+print()
+
+def multiplication():
+    print("This multiplies 4 by b")
+
+    b = int (input("Please enter the value of b:"))
+
+    a = b * 4
+
+    print("The value of a is:",a)
+
+    
+
+multiplication()
+
+print()
+
+
+def division():
+
+    
+    print("This divides th value of a by 3.14")
+
+    a = int(input("Please enter the value of b:"))
+
+    b = a/3.14
+
+    print("The value of b is:",b)
+
+division()
+
+print()
+
+def subtraction():
+    
+    print("This subtracts 8 from b")
+
+    b = int(input("Please enter the value of b:"))
+
+    a = b-8
+
+    print("The value of a is:",a)
+
+
+subtraction()
+            
+
+
+    
+    
