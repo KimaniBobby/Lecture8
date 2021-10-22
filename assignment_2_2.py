2eddab16713ce24a60432a285f4e85f4a308c69e
Bobby Kimani <bobbykimani@Bobbys-MacBook-Pro.local>
2021-10-22 11:37:31 +0300

    Calculates the profit using total revenue

---
+++ assignment_2_2.py
@@ -0,0 +1,22 @@
+#Profit and total revenue
+#assignment_2_2.py
+
+def main():
+    
+    print("This program helps calculate the profit of Company X")
+
+#profit = 23% of total revenue
+
+    total_revenue = int(input("Please enter the total revenue:"))
+
+    profit = 0.23 * total_revenue
+
+    print("The profit for Company X is:",profit)
+
+
+main()
+          
+    
+
+
+    
